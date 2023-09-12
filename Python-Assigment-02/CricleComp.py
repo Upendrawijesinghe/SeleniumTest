144782fd04aca341ab8d528e0a4fea3310281e19
Upendrawijesinghe <144831716+Upendrawijesinghe@users.noreply.github.com>
2023-09-12 23:50:42 +0530

    Add files via upload

---
+++ Python-Assigment-02/CricleComp.py
@@ -0,0 +1,7 @@
+radius =float(input("Enter the radius of cricle :-"))
+pi =3.14
+dia =radius *2
+cir=2*pi*radius
+area =pi*radius*radius
+print ("The diameter of cricle is:-",dia)
+print ("The area of cricle is:-",area)
